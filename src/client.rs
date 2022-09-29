757314e0b167e95fd24e2256a31c1058ffccd28c
Janmajaya Mall <janmajaya.choithram@gmail.com>
2022-09-30 04:50:16 +0530

    added tree for fun

--- src/client.rs
+++ src/client.rs
@@ -145,6 +145,21 @@ fn build_query(
 /// `Ct_odd = Ct_odd * X^-1`
 ///
 /// Therefore now we have
-/// Ct_even = C + Subs(C, N+1) = Enc(Σ 2b_2i • X^2i)
-/// Ct_odd = (C - Subs(C, N+1)) • X^-1 = Enc(Σ 2b_2i+1 • X^2i)
+/// `Ct_even = C + Subs(C, N+1) = Enc(Σ 2b_2i • X^2i)`
+/// `Ct_odd = (C - Subs(C, N+1)) • X^-1 = Enc(Σ 2b_2i+1 • X^2i)`
+///
+/// Now to obtain `Enc(b_i)`s of bits we perform the same operation
+/// recursively. After `log(N)` branches we will have `Enc(N * b_i)`
+/// as N leaf nodes.
+/// `k = n/2^(i-1) + 1`
+///                                    [x0, x1, x2, x3, x4, x5, x6, x7] depth of tree: logN
+///                                      / i = 1                                       \
+///                                     / C + Subs(•, k)                                \ C - Subs(•, k)          
+///                         2^k*[x0, x2, x4, x6], n/2^(k-1) + 1         2^k*[x1, x3, x5, x7] * x^-k => 2*[x0, x2, x4, x6, x8]
+///                                    / i = 2              \
+///                                   / C + Subs(•, k)       \ C - Subs(•, k)          
+///            Subs(2^k*[x0, x4], n/2^(k-1) + 1)          2^k*[x2, x6] * -k
+///                                  / i = 3                    \
+///                                 / C + Subs(•, k)             \ C - Subs(•, k)          
+///                             2^k*[x0]                        2^k*[x4] * x^-k
 fn resolve_query(query_ct: &BfvCipherText, ksks: Vec<Ksk>) {}
